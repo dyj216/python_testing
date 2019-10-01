0c1d90f3af28794ec0c73ca6b5a95d253bb1b66b
Zsolt Endreffy <zsolt.endreffy@balabit.com>
2019-10-01 19:02:57 +0200

    use driver for testing

--- web.py
+++ web.py
@@ -3,14 +3,13 @@ from selenium import webdriver
 
 
 @pytest.fixture
-def selenium_webdriver():
+def driver():
     driver = webdriver.Firefox(executable_path="bin/geckodriver")
     yield driver
     driver.close()
 
 
-def test_chernobyl_accident_date():
-    driver = webdriver.Firefox(executable_path="bin/geckodriver")
+def test_chernobyl_fact(driver, fact):
     driver.get("https://wikipedia.org")
     search_field = driver.find_element_by_id("searchInput")
     search_field.send_keys("chernobyl disaster")
@@ -18,5 +17,13 @@ def test_chernobyl_accident_date():
     submit_button.click()
     body_content = driver.find_element_by_id("bodyContent")
     paragraphs = body_content.find_elements_by_tag_name("p")
-    assert "26 April 1986" in paragraphs[2].text
-    driver.close()
+    assert fact in paragraphs[2].text
+
+
+def test_fukushima(driver):
+    driver.get("https://en.wikipedia.org/wiki/Chernobyl_disaster")
+    link = driver.find_elements_by_xpath("//a[@href='/wiki/Fukushima_Daiichi_nuclear_disaster']")[0]
+    link.click()
+    p = driver.find_element_by_css_selector(".mw-parser-output > p:nth-child(8)")
+    assert "11 March 2011" in p.text
+    return 0
