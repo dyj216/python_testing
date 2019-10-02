abdbf1914a606767c775f85b288a0d8e8dcc077d
Zsolt Endreffy <zsolt.endreffy@balabit.com>
2019-10-02 16:51:49 +0200

    add inotifywait command in comment

--- tdd_test.py
+++ tdd_test.py
@@ -2,9 +2,14 @@ import unittest
 from tdd_calculator import Calculator
 
 
+# Run continuously this test:
+# Install inotify-tools
+# while inotifywait -e close_write .; do python -m unittest tdd_test.py; done
+
+
 class TestCalculator(unittest.TestCase):
     def setUp(self):
         self.calculator = Calculator()
 
     def test_addition(self):
-        self.assertEquals(15, self.calculator.add(7, 8))
+        self.assertEqual(15, self.calculator.add(7, 8))
