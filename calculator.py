778f1abf211e6398fd44d15a26e9699c095284b4
Zsolt Endreffy <zsolt.endreffy@balabit.com>
2019-11-26 11:14:41 +0100

    add mocking example

--- calculator.py
+++ calculator.py
@@ -1,7 +1,9 @@
 import math
+import time
 
 
 class Calculator:
     @staticmethod
     def square_root(a):
+        time.sleep(6)
         return math.sqrt(a)
--- calculator_unittest.py
+++ calculator_unittest.py
@@ -1,4 +1,5 @@
 import unittest
+from unittest.mock import patch
 from calculator import Calculator
 
 
@@ -6,7 +7,8 @@ class TestCalculator(unittest.TestCase):
     def setUp(self):
         self.calculator = Calculator()
 
-    def test_square_root(self):
+    @patch('time.sleep', return_value=None)
+    def test_square_root(self, mocked_sleep):
         for (number, root) in [(4, 2), (9, 3), (16, 4)]:
             with self.subTest(fn_input=number, expected=root):
                 self.assertEqual(root, self.calculator.square_root(number))
